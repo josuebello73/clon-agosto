dbfacab9c69b62ce86b32944ff5c5b86f3e7f47d
josuebello73 <josuebello73@gmail.com>
2026-08-15 10:40:18 -0400

    delete is done

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import (PostListView,
+from .views import (PostDeleteView, 
+                    PostListView,
                     PostDetailView, 
                     PostCreateView, 
                     PostUpdateView,
@@ -9,5 +10,6 @@ urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('post/crear/', PostCreateView.as_view(), name='post_create'),
-    path('post/<int:pk>/actualizar/', PostUpdateView.as_view(), name='post_update'),        
+    path('post/<int:pk>/actualizar/', PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/eliminar/', PostDeleteView.as_view(), name='post_delete'),
 ]
